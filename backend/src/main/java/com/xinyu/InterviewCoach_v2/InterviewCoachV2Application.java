a221bed2ad9f0e6e2a3a3a798f03cd0e2415eee2
huangxinxinyu <xinyuhimself@gmail.com>
2025-08-05 22:46:24 +1000

    first commit

---
+++ backend/src/main/java/com/xinyu/InterviewCoach_v2/InterviewCoachV2Application.java
@@ -0,0 +1,13 @@
+package com.xinyu.InterviewCoach_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCoachV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCoachV2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/xinyu/InterviewCoach_v2/InterviewCoachV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinyu.InterviewCoach_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCoachV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
